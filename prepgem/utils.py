2fba481c8838e6227d97c5a4e88680c0d0cbe307
Mohamed ED Deryouch <88974539+simoderyouch@users.noreply.github.com>
2024-03-18 07:27:53 +0000

    Update utils.py

--- prepgem/utils.py
+++ prepgem/utils.py
@@ -8,6 +8,7 @@ def preprocess_cell(fun, pbar):
 def preprocess_columns(df, columns, preprocessing_function , fun_name):
     df_copy = df.copy()
     for column in columns:
+            print(f"Preprocessing '{column}' with {fun_name}")
             pbar = tqdm(total=df_copy[column].shape[0], desc=f"Preprocessing '{column}' with {fun_name}")
             df_copy[column] = df_copy[column].apply(lambda x: preprocess_cell(preprocessing_function(x), pbar))
             pbar.close()
